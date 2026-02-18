75f9037bb0b15914fab60b89b35e30b9ca7d373a
eiyu18 <eiyuefendi12@gmail.com>
2026-02-18 10:44:13 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return 'Hello World';
 });
+
+Route::get('/world', function () {
+   return 'World';
+});
+
+Route::get('/welcome', function () {
+   return 'Welcome';
+});
+
+Route::get('/about', function () {
+   return 'Eiyu - 244107020181';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'My name'.$name;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'My name ' .$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function 
+($post, $commentId) {
+    return 'Post to'.$post." Comments to: ".$commentId;
+});
+
+Route::get('/articles/{id}', function($id) {
+    return 'Article Page with ID ' .$id;
+});
+
+
+Route::get('/user/profile', function () {
+    //
+})->name('profile');
+
+
+
